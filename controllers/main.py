2d6593b7ff2d49f9863ddaf26f299ae88a1ac0f0
aek <aekroft@gmail.com>
2018-10-02 13:38:23 -0500

    save country_id and state_id

--- controllers/main.py
+++ controllers/main.py
@@ -41,6 +41,18 @@ class AccountSignUpDetails(my_website_sale_auth):
 
         return res
 
+    def do_signup(self, qcontext):
+        """ Shared helper that creates a res.partner out of a token """
+        values = dict((key, qcontext.get(key))
+                      for key in ('login', 'name', 'password', 'country_id', 'state_id'))
+        assert any([k for k in values.values()]
+                   ), "The form was not properly filled in."
+        assert values.get('password') == qcontext.get(
+            'confirm_password'), "Passwords do not match; please retype them."
+        values['lang'] = request.lang
+        self._signup_with_values(qcontext.get('token'), values)
+        request.cr.commit()
+
 
 class AccountSignUpDetailsExt(website_sale):
 
